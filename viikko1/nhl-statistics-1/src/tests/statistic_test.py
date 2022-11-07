ab52cc1ea831efe1756cf0c088aced6c8f5fb7fb
hartonenolli <hartonenolli@gmail.com>
2022-11-07 20:51:01 +0200

    Tehty tehävä 16

---
+++ viikko1/nhl-statistics-1/src/tests/statistic_test.py
@@ -0,0 +1,39 @@
+import unittest
+from statistics import Statistics
+from player import Player
+
+class PlayerReaderStub:
+    def get_players(self):
+        return [
+            Player("Semenko", "EDM", 4, 12),
+            Player("Lemieux", "PIT", 45, 54),
+            Player("Kurri",   "EDM", 37, 53),
+            Player("Yzerman", "DET", 42, 56),
+            Player("Gretzky", "EDM", 35, 89)
+        ]
+
+class TestStatistics(unittest.TestCase):
+    def setUp(self):
+        # annetaan Statistics-luokan oliolle "stub"-luokan olio
+        self.statistics = Statistics(
+            PlayerReaderStub()
+        )
+
+    def test_search_by_name(self):
+        haetaan = self.statistics.search("Semenko")
+        self.assertEqual(haetaan.name, "Semenko")
+
+    def test_find_player_team(self):
+        haetaan = self.statistics.team("EDM")
+        self.assertEqual(haetaan[1].name, "Kurri")
+
+    def test_find_player_wrong_name(self):
+        haetaan = self.statistics.search("Hartonen")
+        self.assertEqual(haetaan, None)
+    
+    def test_top(self):
+        haetaan = self.statistics.top(4)
+        top5 = [haetaan[0].name, haetaan[1].name, haetaan[2].name,
+            haetaan[3].name
+            ]
+        self.assertEqual(top5, ["Gretzky", "Lemieux", "Yzerman", "Kurri"])
