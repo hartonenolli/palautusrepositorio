f926046a2eb59e906b7e1bb736d36b85b3700a0c
hartonenolli <hartonenolli@gmail.com>
2022-11-21 13:19:26 +0200

    Tehtävä3

--- viikko3/nhl-reader/src/index.py
+++ viikko3/nhl-reader/src/index.py
@@ -1,6 +1,48 @@
 import requests
 from player import Player
 
+class PlayerReader:
+    def __init__(self, url):
+        self.url = requests.get(url).json()
+
+class PlayerStats:
+    def __init__(self, response):
+        self.response = response
+        #self.players = []
+
+    def top_scorers_by_nationality(self, nationality):
+        players = []
+        response_values = []
+        for values in self.response:
+            #if values.get('nationality') == 'FIN':
+            if 'FIN' in values.get('nationality'):
+                response_values.append(values)
+
+        for player_dict in response_values:
+            player = Player(
+                player_dict['name'],
+                player_dict['team'],
+                player_dict['goals'],
+                player_dict['assists'],
+                player_dict['nationality']
+            )
+            players.append(player)
+
+
+        list = []
+        isoin = 0
+        isoin_pelaaja = None
+        while players != []:
+            for player in players:
+                arvo = player.molemmat
+                if arvo >= isoin:
+                    isoin = arvo
+                    isoin_pelaaja = player
+            list.append(isoin_pelaaja)
+            players.remove(isoin_pelaaja)
+            isoin = 0
+        return list
+
 def filter_players(players):
     list = []
     isoin = 0
@@ -18,29 +60,9 @@ def filter_players(players):
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2021-22/players"
-    response = requests.get(url).json()
-
-    #print("JSON-muotoinen vastaus:")
-    #print(response)
-
-    players = []
-    response_values = []
-    for values in response:
-        if values.get('nationality') == 'FIN':
-            response_values.append(values)
-
-    for player_dict in response_values:
-        player = Player(
-            player_dict['name'],
-            player_dict['team'],
-            player_dict['goals'],
-            player_dict['assists'],
-            player_dict['nationality']
-        )
-        players.append(player)
-
-    #print("Oliot:")
-    filtered_list = filter_players(players)
+    response = PlayerReader(url)
+    stats = PlayerStats(response.url)
+    filtered_list = stats.top_scorers_by_nationality("FIN")
 
     for player in filtered_list:
         print(player)
