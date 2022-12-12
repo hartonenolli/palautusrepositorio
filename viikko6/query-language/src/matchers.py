4e494c2be4c960580c92852b425bba6c460935b9
hartonenolli <hartonenolli@gmail.com>
2022-12-12 17:24:15 +0200

    Tehtävä3

--- viikko6/query-language/src/matchers.py
+++ viikko6/query-language/src/matchers.py
@@ -38,6 +38,7 @@ class Not:
         self.olio = olio
 
     def matches(self, argument):
+        #print(self.olio._attr)
         if argument.goals < self.olio._value:
             return True
 
@@ -50,3 +51,23 @@ class HasFewerThan:
         player_value = getattr(player, self._attr)
 
         return player_value < self._value
+
+class Or:
+    def __init__(self, *oliot):
+        self.oliot = oliot
+
+    def matches(self, argument1):
+        for olio in self.oliot:
+            try:
+                if olio._attr == "goals":
+                    if argument1.goals >= olio._value:
+                        return True
+                elif olio._attr == "assists":
+                    if argument1.assists >= olio._value:
+                        return True
+            except Exception:
+                break
+
+        for olio in self.oliot:
+            if olio._team == argument1.team:
+                return True
