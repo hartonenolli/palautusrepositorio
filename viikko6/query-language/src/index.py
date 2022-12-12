cb0f05051bc3f994f0912bdd257ca4de3aa81d13
hartonenolli <hartonenolli@gmail.com>
2022-12-12 21:49:53 +0200

    Tehtävä4

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -2,23 +2,40 @@ from statistics import Statistics
 from player_reader import PlayerReader
 from matchers import And, HasAtLeast, PlaysIn, All, Not, HasFewerThan, Or
 
-def main():
-    url = "https://studies.cs.helsinki.fi/nhlstats/2021-22/players.txt"
-    reader = PlayerReader(url)
-    stats = Statistics(reader)
+class Lana:
+    def main():
+            url = "https://studies.cs.helsinki.fi//nhlstats/2021-22/players.txt"
+            reader = PlayerReader(url)
+            stats = Statistics(reader)
 
-    matcher = And(
-        HasAtLeast(70, "points"),
-        Or(
-            PlaysIn("NYR"),
-            PlaysIn("FLA"),
-            PlaysIn("BOS")
-        )
-    )
+            query = QueryBuilder()
+            #matcher = query.build()
+            #matcher = query.playsIn("NYR").build()
+            matcher = query.playsIn("NYR").hasAtLeast(10, "goals").hasFewerThan(20, "goals").build()
 
-    for player in stats.matches(matcher):
-        print(player)
+            for player in stats.matches(matcher):
+                print(player)
 
+
+class QueryBuilder:
+    def __init__(self, pino=Lana()):
+        self.pino_olio = pino
+
+    def build(self):
+        if self.pino_olio == None:
+            filtered_with_all = All()
+            return filtered_with_all
+        return self.pino_olio
+
+    def playsIn(self, team):
+        return QueryBuilder(PlaysIn(team))
+    
+    def hasAtLeast(self, point, arg):
+        return QueryBuilder(HasAtLeast(point, arg))
+    
+    def hasFewerThan(self, point, arg):
+        return QueryBuilder(HasFewerThan(point, arg))
 
 if __name__ == "__main__":
-    main()
+    osuva = Lana
+    osuva.main()
